105f2f7d933884d8527d9ea6a1963da591a79298
Tomate0613 <69756782+Tomate0613@users.noreply.github.com>
2025-06-28 08:18:50 +0200

    add minecraft flake

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Minecraft";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+
+    let
+      inherit (nixpkgs) lib;
+
+      systems = lib.systems.flakeExposed;
+      forAllSystems = lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              jdk21
+            ];
+
+            __GL_THREADED_OPTIMIZATIONS = 0;
+            LD_LIBRARY_PATH = lib.makeLibraryPath (
+              with pkgs;
+              [
+                (lib.getLib stdenv.cc.cc)
+
+                ## native versions
+                glfw3-minecraft
+                openal
+
+                ## openal
+                alsa-lib
+                libjack2
+                libpulseaudio
+                pipewire
+
+                ## glfw
+                libGL
+                libX11
+                libXcursor
+                libXext
+                libXrandr
+                libXxf86vm
+
+                udev # oshi
+
+                flite # Text to speech (Otherwise minecraft will log an error every time it launches)
+              ]
+            );
+          };
+        }
+      );
+    };
+}
