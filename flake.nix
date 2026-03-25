42b15bee904ddf4f0f8c81faf2e3ae351303a5d8
Tomate0613 <69756782+Tomate0613@users.noreply.github.com>
2026-03-25 18:10:21 +0100

    add spyglassmc flake

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Spyglassmc";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+
+    let
+      inherit (nixpkgs) lib;
+
+      systems = lib.systems.flakeExposed;
+      forAllSystems = lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          default = pkgs.buildNpmPackage {
+            pname = "spyglassmc-language-server";
+            version = "1.0.0";
+
+            src = ./.;
+
+            npmDepsHash = "sha256-bnSHIcyAkjEoILcoad85m4onsim31OmwGH3bkbDnjXg=";
+            npmDepsFetcherVersion = 2;
+
+            dontNpmBuild = true;
+
+            postInstall = ''
+              mkdir -p $out/bin
+
+              makeWrapper ${lib.getExe pkgs.nodejs} $out/bin/spyglassmc-language-server \
+                --inherit-argv0 \
+                --add-flags $out/lib/node_modules/spyglassmc-language-server/node_modules/@spyglassmc/language-server/bin/server.js
+            '';
+          };
+        }
+      );
+    };
+}
