e8b749678400b3115aa20e87d1e13c9a2f129e79
Tomate0613 <69756782+Tomate0613@users.noreply.github.com>
2026-04-01 22:31:27 +0200

    include jmc

--- flake.nix
+++ flake.nix
@@ -6,36 +6,71 @@
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { nixpkgs, self }:
 
     let
       inherit (nixpkgs) lib;
 
       systems = lib.systems.flakeExposed;
       forAllSystems = lib.genAttrs systems;
+
+      nixpkgsFor = forAllSystems (system: nixpkgs.legacyPackages.${system});
     in
     {
       devShells = forAllSystems (
         system:
         let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
+          pkgs = self.packages.${system};
         in
         {
           default = pkgs.mkShell {
-            packages = with pkgs; [ jdk25 ];
-
-            env = {
-
-              LD_LIBRARY_PATH = lib.makeLibraryPath (
-                with pkgs;
-                [
-                  glib
-                ]
-              );
-            };
+            packages = with pkgs; [ default ];
           };
+        }
+      );
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        rec {
+          jmc = pkgs.stdenv.mkDerivation {
+            pname = "jmc";
+            version = "9.1.1";
+
+            src = pkgs.fetchurl {
+              url = "https://github.com/adoptium/jmc-build/releases/download/9.1.1/org.openjdk.jmc-9.1.1-linux.gtk.x86_64.tar.gz";
+              hash = "sha256-ARYSN8f4g10oLN7G3NL0XIIeDGmgI4Ptzt0nf1WnI34=";
+            };
+
+            nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+
+            buildInputs = with pkgs; [
+              (lib.getLib stdenv.cc.cc)
+              glib
+              gtk3
+              makeWrapper
+            ];
+
+            autoPatchelfIgnoreMissingDeps = true;
+
+            unpackPhase = ''
+              tar -xzf $src
+              ls
+            '';
+
+            installPhase = ''
+              mkdir -p $out/opt/jmc
+              cp -r * $out/opt/jmc
+
+              mkdir -p $out/bin
+
+              makeWrapper $out/opt/jmc/"JDK Mission Control"/jmc $out/bin/jmc \
+                --set LD_LIBRARY_PATH "${lib.makeLibraryPath (with pkgs; [ glib ])}"
+            '';
+          };
+
+          default = jmc;
         }
       );
     };
